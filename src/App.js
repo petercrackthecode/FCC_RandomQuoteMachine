3915f56077a769835d40cdcd58b3b5237476c870
Peter Nguyen <nguyenvandung02051999@gmail.com>
2019-03-18 17:46:31 -0700

    Change files organization

--- src/App.js
+++ src/App.js
@@ -2,24 +2,29 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import {getRandomColor} from './functions/getRandomColor.js';
+
+// Boostrap
+import Button from 'react-bootstrap/Button';
+
+import {QuoteBox} from './Components/quote-box.js';
+
 class App extends Component {
+  state= {
+      backgroundColor: 'brown'
+  };
+
+  handleColorChange= () => {
+    this.setState({
+      backgroundColor: getRandomColor()
+    });
+  }
+
   render() {
+    const backgroundColor= this.state.backgroundColor;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="App" style={{backgroundColor: backgroundColor}}>
+        <QuoteBox onColorChange={this.handleColorChange}/>
       </div>
     );
   }
---
+++ src/Components/quote-box.js
@@ -0,0 +1,5 @@
+import React, {Component} from 'react';
+
+class QuoteBox extends React.Component {
+  
+}
