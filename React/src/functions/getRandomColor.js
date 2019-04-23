a636096612710c4818eca851efcf59031e583dc6
Peter Nguyen <nguyenvandung02051999@gmail.com>
2019-04-22 23:17:14 -0700

    Change some files organization

--- React/src/functions/getRandomColor.js
+++ React/src/functions/getRandomColor.js
@@ -4,7 +4,7 @@ export const getRandomColor= () => {
 
   while (color === '#' || color === '#FFFFFF') {
     for (let index= 0; index < 6; index++)  {
-      color+= letters[Math.floor(Math.random() * 16)];
+      color+= letters[Math.floor(Math.random() * 15)];
     }
   }
 
